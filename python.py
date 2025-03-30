f9c52e02346747d45f024db260a1fc0da42ad8fc
azhartheking <azhario786786@gmail.com>
2025-03-30 12:31:13 +0800

    Update python.py

--- python.py
+++ python.py
@@ -53,7 +53,7 @@ elif view_option == "Lecturer Information":
     if os.path.exists(lecturer_img_path):
         st.write("An Outstanding Educator, Researcher, and Innovator")
         lecturer_img = Image.open(lecturer_img_path)
-        st.image(lecturer_img, caption="Lecturer: Ts. ChM. Dr. Mohd Dzul Hakim Wirzal", use_column_width=True)
+        st.image(lecturer_img, caption="Lecturer: Ts. ChM. Dr. Mohd Dzul Hakim Wirzal",  use_container_width=True)
     else:
         st.error("Error: Lecturer image not found. Please check the file path.")
     
@@ -62,7 +62,7 @@ elif view_option == "Lecturer Information":
     st.write("In recognition of his exceptional work, Dr. Hakim has been honored with numerous prestigious awards and accolades. He was recently awarded an Honorary Professorship from Karshi Engineering-Economics Institute, Uzbekistan (2024) and has been invited as a Fellow Teacher under the ERASMUS Grant at UCBL, France, and Oita Kosen, Japan. His commitment to both research and education has earned him multiple Gold Awards for Research Grants (2022, 2024) and Gold and Silver Medals for teaching innovations (2020). Furthermore, his contributions to student development were recognized with the Outstanding Student Development Award (2023), solidifying his impact on shaping future engineers and researchers.")
     if os.path.exists(lecturer_img_path_1):
         lecturer_img = Image.open(lecturer_img_path_1)
-        st.image(lecturer_img,use_column_width=True)
+        st.image(lecturer_img, use_container_width=True)
     st.write("Dr. Hakim is also highly regarded for his international collaborations, working closely with renowned institutions such as NTNU (Norway), USP (Brazil), PRSB, UCBL (France), UCLL (Belgium), and KEEI (Uzbekistan). His active involvement in cross-border research and academic exchange programs has significantly contributed to scientific knowledge, technological advancements, and global cooperation in chemical engineering. ")
     st.write("Beyond his research and academic pursuits, Dr. Hakim plays a crucial role in mentoring and guiding students, inspiring them to excel in their respective fields. His ability to bridge theoretical knowledge with industrial applications makes him a highly respected figure among both students and fellow academics. His passion for research, education, and innovation continues to drive progress in chemical engineering, making a lasting impact on both the academic community and the industry. ")
     st.write("A true inspiration and a remarkable leader in the field, Dr. Hakim’s unwavering dedication to excellence sets an outstanding example for aspiring engineers and researchers worldwide  ")
