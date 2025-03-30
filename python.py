1e323880a73ec1ec896b9307fb1aac8885335b02
azhartheking <azhario786786@gmail.com>
2025-03-30 22:14:35 +0800

    Update python.py

--- python.py
+++ python.py
@@ -59,7 +59,7 @@ elif view_option == "Course Introduction":
     st.write("This course is a required component of core discipline in Chemical Engineering profession.  It is designed to equip engineers with the fundamental of analytical chemistry.  This course will help Engineers to take up challenges in analyzing and evaluating samples in chemical industries. ")
     selected_chapter = st.selectbox("Select a chapter to view Notes", 
                                 ["Chapter 1", "Chapter 2", "Chapter 3", "Chapter 4", 
-                                 "Chapter 5", "Chapter 6", "Chapter 7", "Chapter 8"], 
+                                 "Chapter 5", "Chapter 6", "Chapter 7", "Chapter 8" , "Chapter 9"], 
                                 index=0)
     
     pdf_file = f"Notes/Chapter_{selected_chapter.split(' ')[1]}.pdf"
